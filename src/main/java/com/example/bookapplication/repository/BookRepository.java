beab1f573860444fa5f725560e85ad00833691dc
Шилов Владислав <95354582+Mayflowerpy@users.noreply.github.com>
2024-09-26 12:47:37 +0300

    repo явный поиск в листе

--- src/main/java/com/example/bookapplication/repository/BookRepository.java
+++ src/main/java/com/example/bookapplication/repository/BookRepository.java
@@ -3,11 +3,14 @@ package com.example.bookapplication.repository;
 import com.example.bookapplication.entity.Author;
 import com.example.bookapplication.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> getBooksByAuthor(Author author);
+    @Query("SELECT b FROM Book b JOIN b.authorList a WHERE a = :author")
+    List<Book> getBooksByAuthor(@Param("author") Author author);
 }
